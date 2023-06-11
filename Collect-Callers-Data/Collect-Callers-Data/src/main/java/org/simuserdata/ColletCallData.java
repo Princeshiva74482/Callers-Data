fec0803bee68631932b60452b4e66d7d5b4884e2
Princeshiva74482 <shiva74482@outlook.com>
2023-06-11 21:50:49 +0530

    Added Application Model.

---
+++ Collect-Callers-Data/Collect-Callers-Data/src/main/java/org/simuserdata/ColletCallData.java
@@ -0,0 +1,11 @@
+package org.simuserdata;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ColletCallData {
+    public static void main(String[] args) {
+        SpringApplication.run(ColletCallData.class, args);
+    }
+    }
